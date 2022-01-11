9a3fa1b4fdb01d3b92b7c893be98121a57e4ca7d
kate-turnbull <kate@kateturnbull.codes>
2022-01-12 11:38:37 +1300

    Get the server up and running

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+ 
 const express = require('express')
 const hbs = require('express-handlebars')
 
 const server = express()
+
+module.exports = server
 
 // Server configuration
 server.use(express.static('public'))
@@ -12,6 +15,9 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
+server.get('/', (req, res) => {
+    res.send('Pupparazzi')
+  })
 
 
 module.exports = server
