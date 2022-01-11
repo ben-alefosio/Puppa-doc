8e08f1f37e8da5277f12b5018f24509e95a3cdb4
jinny <vedaforest@gmail.com>
2022-01-12 11:49:17 +1300

    render

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const { render } = require('express/lib/response')
+const fs = require('fs')
+const path = require('path')
 
 const server = express()
 
@@ -12,6 +15,22 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-
-
 module.exports = server
+
+// Bring data 
+const dogData = require('./data.json')
+
+
+
+server.get('/', (req, res) => {
+    const viewData = {
+        puppies: dogData.puppies
+    }
+    res.render('home', viewData)
+})
+
+server.get('/images/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const foundId = dogData.find(pic => pic.id === id)
+    res.render('images', foundId)
+})
