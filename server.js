479226a4dec1df743501ad16c88c45953ebdb582
kate-turnbull <kate@kateturnbull.codes>
2022-01-12 14:59:36 +1300

    Display puppy data

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
  
 const express = require('express')
 const hbs = require('express-handlebars')
+const fs = require('fs').promises
+// const data = require('./data.json')
 
 const server = express()
 
-module.exports = server
 
 // Server configuration
 server.use(express.static('public'))
@@ -16,21 +17,33 @@ server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
 server.get('/', (req, res) => {
-    res.send('Pupparazzi')
+
+    fs.readFile('data.json', 'utf-8')
+
+    .then((puppies) => { 
+      JSON.parse(puppies)
+      console.log(puppies);
+    })
+    .catch(err => {
+      console.error('ops something happend ', err);
+    })
+
+    // res.render('home', viewData)
   })
 
   // write fs.promise readfile function 
-  const fs = require('fs').promises
+ 
   
-  const p = fs.readFile('data.json', 'utf-8')
-  .then(() => {
-    console.log('puppy file has been saved');
-  })
-  .catch(err => {
-    console.error('ops something happend ', err);
-  })
 
-  console.log(p)
+
+// const p = fs.readFile(puppyData, 'utf-8)
+
+//   const p = JSON.parse(fs.readFile('data.json', 'utf-8'))
+
+
+  //console.log(JSON.parse(p))
+  console.log('Hi')
+
 
 
 module.exports = server
