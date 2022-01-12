25b8400ae58d3826433b9d0f3e57eb024c56edad
jinny <vedaforest@gmail.com>
2022-01-12 21:31:04 +1300

    data updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = express()
 
 // Server configuration
 server.use(express.static('public'))
-server.use(express.urlencoded({ extended: false }))
+server.use(express.urlencoded({ extended: false }));
 
 // Handlebars configuration
 server.engine('hbs', hbs({ extname: 'hbs' }))
@@ -33,7 +33,7 @@ server.get('/puppies/:id', (req, res) => {
     const id = Number(req.params.id)
 
     const foundObj = dogData.puppies.find(obj => obj.id == id)
-    console.log(dogData.puppies)
+
     res.render('details', foundObj)
 })
 
@@ -47,21 +47,34 @@ server.get('/puppies/:id/edit', (req, res) => {
 
 
 server.post('/puppies/:id/edit', (req, res) => {
-    const id = Number(req.params.id)
-    const foundObj = dogData.puppies.find(obj => obj.id === id)
+    const paramsid = Number(req.params.id)
 
-    const newdata = JSON.stringify(req.body, null, 2)
+    const newdata = JSON.parse(JSON.stringify(req.body, null, 2))
+    newdata.id = paramsid
 
-    const changedData = dogData.filter((obj) => { 
-        obj.id !== newdata.id
-        
-    })
+    dogData.puppies = dogData.puppies.filter(obj => obj.id !== paramsid)
 
-    console.log({...changedData})
+    const list = dogData.puppies
+
+    dogData.puppies = [
+        ...list,
+        { ...newdata }
+    ]
+
+    console.log('Dogdata from global', dogData)
+
+
+    const filePath = path.join(__dirname, '/data.json')
+    // const cat = ['cat']
+
+    fs.writeFile(filePath, JSON.stringify(dogData, null, 2), 'utf8')
+        .then(() => {
+            console.log('doonnnneeee')
+        })
 
 
 
 
 
-    res.redirect(`/puppies/${foundObj}`)
+    res.redirect(`/puppies/${paramsid}`)
 })
