8d5a754bc2a7f297cbb012c59dc18ed9a8a6bebc
angela-yen <angela.yen0210@gmail.com>
2022-01-12 17:09:49 +1300

    gave it a go

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
+server.get('/', (req, res) => {
+  const filepath = path.join(__dirname, 'data.json')
 
+  const pups = JSON.parse(data.Puppies)
+  const viewData = {
+    Puppies: pups
+  }
+  res.render('home', viewData)
+})
 
 module.exports = server
