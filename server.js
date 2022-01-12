163c56279f7e3ae944dbc5946335988774ca8db2
gus.hawke <gus.hawke@gmail.com>
2022-01-12 15:34:00 +1300

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const res = require('express/lib/response')
+const { get } = require('express/lib/response')
+
+const router = express.Router()
+const fs = require('fs').promises
+const fsPromises = fs
 
 const server = express()
 
@@ -13,5 +19,17 @@ server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
 
+server.get('/', (req, res) => {
+  res.send('pupparazi')
+})
+
+// fsPromises.readFile('/data', 'utf-8')
+
+//   .then(() => {
+//     console.log()
+//   })
+//   .catch(err => {
+//     console.error('ooopsy daisy', err)
+//   })
 
 module.exports = server
