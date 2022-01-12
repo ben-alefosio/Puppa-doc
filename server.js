96b0e846a33e2c886fa5a95861e38115de2607d0
Qian Han <kategreat1@hotmail.com>
2022-01-12 16:37:34 +1300

    show puppies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-
+const fs = require('fs').promises
 const server = express()
 
 // Server configuration
@@ -12,37 +12,21 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-
-module.exports = server
-
-// 1. In the server.js, add an HTTP GET root route (/). For now, let's just send the word 'Pupparazzi'
-// Start the server and go to http://localhost:3000 to see if we are winning
-
 server.get('/', (req, res) => {
-  res.send('Hey Pupparazzi')
+  fs.readFile('data.json', 'utf-8')
+    .then((puppies) => {
+      const puppiesJson = JSON.parse(puppies)
+      console.log(puppies)
+      const viewData = {
+        puppies: puppiesJson.puppies
+      }
+      console.log(viewData)
+      res.render('home', viewData)
+      return null
+    })
+    .catch(err => {
+      console.error('ops something happend ', err)
+    })
 })
 
-//   2. In our server file, change the GET / route function. We will use this route to:
-
-// read the puppies from our data.json file using fsPromises.readFile (don't forget to parse the data into a JavaScript object)
-// render the puppies using the home view (that has already been created) and your puppies data
-// If your page renders, but there are no puppies:
-
-// check what data the view is expecting to receive
-// console.log the view data object you are passing to the render and make sure this matches what the view is expecting
-
-const fs = require('fs').promises
-// const fs = require('fs/promises')
-
-// reading files using fs/promises
-const p = fs.readFile('data.json', 'utf-8')
-  .then(() => {
-    console.log('file is saved')
-    return null
-  })
-  .catch(err => {
-    console.error('ops something happend ', err)
-  })
-// writing to files
-
-console.log(p)
+module.exports = server
