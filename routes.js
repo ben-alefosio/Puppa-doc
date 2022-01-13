5bb88d5b0dd5b92c2ff57ff8e3bdd44a4d74648d
Sam Wylie <sam.wylie20@gmail.com>
2022-01-13 17:41:10 +1300

    Second route file working

--- routes.js
+++ routes.js
@@ -1,9 +1,32 @@
-const server = require('./server')
-const fsPromises = require('fs').promises
+const fs = require('fs/promises')
+const express = require('express')
 
-server.get('/puppies/:id', (req, res) => {
-  const id = Number(req.params.id) - 1
-  fsPromises.readFile('./data.json', 'utf-8')
+const router = express()
+
+module.exports = router
+
+router.get('/', (req, res) => {
+  //const id = Number(req.params.id) 
+  fs.readFile('./data.json', 'utf-8')
+    .then(function (result) {
+      const parsedPups = JSON.parse(result)
+      console.log(`I am here ${parsedPups}`)
+      console.log(parsedPups)
+      const template = 'home'
+      const viewData = {
+        puppies: parsedPups.puppies
+      }
+      res.render(template, viewData)
+      return null
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+})
+
+router.get('/puppies/:id', (req, res) => {
+  const id = Number(req.params.id) 
+  fs.readFile('./data.json', 'utf-8')
     .then(function (result) {
       const parsedPups = JSON.parse(result)
       console.log(`I am here ${parsedPups}`)
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-const fsPromises = require('fs').promises
-// const routes = require('./routes.js')
-
-// const datas = require('./data.json')
-// console.log(datas.puppies)
-// console.log(datas.puppies[0].image)
+const routes = require('./routes')
 
 const server = express()
 
 // Server configuration
+
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
 
@@ -18,24 +14,27 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-server.get('/', (req, res) => {
-  fsPromises.readFile('./data.json', 'utf-8')
-    .then(function (result) {
-      const parsedPups = JSON.parse(result)
-      console.log(`I am here ${parsedPups}`)
-      console.log(parsedPups)
-      const template = 'home'
-      const viewData = {
-        puppies: parsedPups.puppies
-      }
-      res.render(template, viewData)
-      return null
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
-})
+server.use(routes)
 
-// server.use(routes)
+// server.get('/', (req, res) => {
+//   fsPromises.readFile('./data.json', 'utf-8')
+//     .then(function (result) {
+//       const parsedPups = JSON.parse(result)
+//       console.log(`I am here ${parsedPups}`)
+//       console.log(parsedPups)
+//       const template = 'home'
+//       const viewData = {
+//         puppies: parsedPups.puppies
+//       }
+//       res.render(template, viewData)
+//       return null
+//     })
+//     .catch(function (error) {
+//       console.log(error)
+//     })
+// })
+
+// // server.use(routes)
 
 module.exports = server
+
