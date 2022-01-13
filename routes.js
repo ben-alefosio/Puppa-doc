e49fecedf6b08477e6ff429aba608e548e9e3626
joanne-lai03 <joanne.lai92@gmail.com>
2022-01-13 23:47:14 +1300

    Attempt to render puppy

--- routes.js
+++ routes.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const { reset } = require('nodemon')
 const router = express.Router()
-const fs = require('fs')
+const fsPromises = require('fs').promises
 
 module.exports = router
 
 router.get('/:id', (req, res) => {
-  res.send('This is working!')
+  // res.send('This is working!' + req.params.igaad)
+  fsPromises.readFile('data.json', 'utf8')
+    .then((puppy) => {
+      const realPuppyData = JSON.parse(puppy)
+      const id = req.params.id
+      res.render('details', realPuppyData[id])
+      return null
+    })
+    .catch(err => {
+      console.log(err)
+    })
 })
