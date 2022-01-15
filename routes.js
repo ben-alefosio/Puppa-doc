5a680af920bd13cbddbe0d90554bb3ed01229212
Chihiro0420 <chihiroe2510@gmail.com>
2022-01-16 12:07:37 +1300

    update router

--- routes.js
+++ routes.js
@@ -4,6 +4,8 @@ const fs = require('fs').promises
 
 module.exports = router
 
+// Accept URL encoded data on POST request
+router.use(express.urlencoded({ extended: false }))
 
 // << Routes for pupiies>> 
 router.get('/puppies/:id', (req, res) => {
@@ -50,14 +52,6 @@ router.get('/puppies/:id/edit', (req, res) => {
       const data = JSON.parse(puppsData)
       const pupdata = data.puppies.find(item => item.id - 1 === id)
 
-      const viewData = {
-        name: data.puppies[id].name,
-        breed: data.puppies[id].breed,
-        owner: data.puppies[id].owner,
-        image: data.puppies[id].image,
-        id: data.puppies[id]
-      }
-
       res.render('edit', pupdata)
     })
 
@@ -65,3 +59,16 @@ router.get('/puppies/:id/edit', (req, res) => {
       console.err('Opps something happend')
     })
 })
+
+// << post route>>
+// 1. Create an object of the updated puppy data from the request body
+// 2. Read in the JSON file and locate the puppy we are going to update
+// 3. Update the puppy in the array
+// 4. Write the entire array back into the JSON file
+// 5. Redirect to the GET / puppies /: id route
+
+router.post('/puppies/:id/edit', (req, res) => {
+  console.log(req.body)
+  res.send('hey')
+})
+
