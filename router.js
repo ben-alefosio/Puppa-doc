42e3a95a4cc25e5ed8c5ed2a28a22c132a163beb
jinny <vedaforest@gmail.com>
2022-01-15 23:44:18 +1300

    refectoring code renew...

--- router.js
+++ router.js
@@ -1,85 +1,85 @@
-// const fs = require('fs').promises
-// const express = require('express')
+const fs = require('fs').promises
+const express = require('express')
 
-// const router = express.Router()
+const router = express.Router()
 
-// module.exports = router
-
-
-// router.get('/', (req, res) => {
-//     fs.readFile('data.json', 'utf-8')
-//         .then(data => {
-//             return JSON.parse(data)
-//         })
-//         .then(parsedData => {
-//             return res.render('home', parsedData)
-//         })
-//         .catch(err => {
-//             console.error(err.message)
-//         })
-// })
+module.exports = router
 
 
-// router.get('/puppies/:id', (req, res) => {
-//     const id = Number(req.params.id)
+router.get('/', (req, res) => {
+    fs.readFile('data.json', 'utf-8')
+        .then(data => {
+            return JSON.parse(data)
+        })
+        .then(parsedData => {
+            return res.render('home', parsedData)
+        })
+        .catch(err => {
+            console.error(err.message)
+        })
+})
 
-//     fs.readFile('data.json', 'utf-8')
-//         .then(data => {
-//             const foundObj = data.puppies.find(obj => obj.id == id)
-//             console.log(data, foundObj)
-//             return JSON.parse(data)
-//         })
-//         .then(parsedData => {
-//             return res.render('detail', parsedData)
-//         })
-//         .catch(err => {
-//             console.error(err.message)
-//         })
+router.get('/puppies/:id', (req, res) => {
+    const id = Number(req.params.id)
 
-// })
+    fs.readFile('data.json', 'utf-8')
+        .then(data => {
+            return JSON.parse(data)
+        })
+        .then(parsedData => {
+            const foundObj = parsedData.puppies.find(obj => obj.id == id)
+            return res.render('details', foundObj)
+        })
+        .catch(err => {
+            console.error(err.message)
+        })
 
+})
 
-// router.get('/puppies/:id/edit', (req, res) => {
-//     const id = Number(req.params.id)
-
-//     fs.readFile('data.json', 'utf-8')
-//         .then(data => {
-//             const foundObj = data.puppies.find(obj => obj.id == id)
-//             console.log(data, foundObj)
-//             return JSON.parse(foundObj)
-//         })
-//         .then(parsedData => {
-//             return res.render('edit', foundObj)
-//         })
-//         .catch(err => {
-//             console.error(err.message)
-//         })
-
-// })
-
-
-
-// router.post('/puppies/:id/edit', (req, res) => {
-//     const paramsid = Number(req.params.id)
-
-//     const newdata = JSON.parse(JSON.stringify(req.body, null, 2))
-//     newdata.id = paramsid
-
-//     dogData.puppies = dogData.puppies.filter(obj => obj.id !== paramsid)
-
-//     const list = dogData.puppies
-//     dogData.puppies = [
-//         ...list,
-//         { ...newdata }
-//     ]
-
-//     const filePath = path.join(__dirname, '/data.json')
+router.get('/puppies/:id/edit', (req, res) => {
+    const id = Number(req.params.id)
+    fs.readFile('data.json', 'utf-8')
+        .then(data => {
+            return JSON.parse(data)
+        })
+        .then(parsedData => {
+            const foundObj = parsedData.puppies.find(obj => obj.id == id)
+            return res.render('edit', foundObj)
+        })
+        .catch(err => {
+            console.error(err.message)
+        })
+})
 
 
-//     fs.writeFile(filePath, JSON.stringify(dogData, null, 2), 'utf8')
-//         .catch((err) => {
-//             console.error(err);
-//         })
+router.post('/puppies/:id/edit', (req, res) => {
+    const id = Number(req.params.id)
 
-//     res.redirect(`/puppies/${paramsid}`)
-// })
+    fs.readFile('data.json', 'utf-8')
+        .then(data => {
+            return JSON.parse(data)
+        })
+        .then(parsedData => {
+
+            const newdata = JSON.parse(JSON.stringify(req.body, null, 2))
+            newdata.id = id
+
+            const filteredData = parsedData.puppies.filter(obj => obj.id !== id)
+
+            const newPuppies = [
+                ...filteredData,
+                { ...newdata }
+            ]
+            parsedData.puppies = newPuppies
+            parsedData.puppies.sort((a, b) => (a.id > b.id ? 1 : -1))
+            return parsedData
+        })
+        .then((parsedData) => {
+            fs.writeFile('data.json', JSON.stringify(parsedData, null, 2), 'utf8')
+            return res.redirect(`/puppies/${id}`)
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+
+})
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 // const { render } = require('express/lib/response')
-const fs = require('fs').promises
+// const fs = require('fs').promises
 const server = express()
 
 // Server configuration
@@ -17,126 +17,7 @@ module.exports = server
 
 // Your routes/router(s) should go here
 
-// const router = require('./router')
+const routertest = require('./router')
 
-// server.use('router', router)
-
-server.get('/', (req, res) => {
-    fs.readFile('data.json', 'utf-8')
-        .then(data => {
-            return JSON.parse(data)
-        })
-        .then(parsedData => {
-            console.log(parsedData);
-            return res.render('home', parsedData)
-        })
-        .catch(err => {
-            console.error(err.message)
-        })
-})
-
-server.get('/puppies/:id', (req, res) => {
-    const id = Number(req.params.id)
-
-    fs.readFile('data.json', 'utf-8')
-        .then(data => {
-            return JSON.parse(data)
-        })
-        .then(parsedData => {
-            const foundObj = parsedData.puppies.find(obj => obj.id == id)
-            return res.render('details', foundObj)
-        })
-        .catch(err => {
-            console.error(err.message)
-        })
-
-})
-
-// server.get('/puppies/:id', (req, res) => {
-//     const id = Number(req.params.id)
-
-//     const foundObj = dogData.puppies.find(obj => obj.id == id)
-
-//     res.render('details', foundObj)
-// })
-
-server.get('/puppies/:id/edit', (req, res) => {
-    const id = Number(req.params.id)
-    fs.readFile('data.json', 'utf-8')
-        .then(data => {
-            return JSON.parse(data)
-        })
-        .then(parsedData => {
-            const foundObj = parsedData.puppies.find(obj => obj.id == id)
-            return res.render('edit', foundObj)
-        })
-        .catch(err => {
-            console.error(err.message)
-        })
-})
-
-// server.get('/puppies/:id/edit', (req, res) => {
-//     const id = Number(req.params.id)
-//     const foundObj = dogData.puppies.find(obj => obj.id == id)
-
-//     res.render('edit', foundObj)
-// })
-
-
-
-// server.post('/puppies/:id/edit', (req, res) => {
-//     const id = Number(req.params.id)
-
-//     fs.readFile('data.json', 'utf-8')
-//         .then(data => {
-//             return JSON.parse(data)
-//         })
-//         .then(parsedData => {
-
-//             const newdata = JSON.parse(JSON.stringify(req.body, null, 2))
-//             newdata.id = id
-
-//             const filteredData = parsedData.puppies.filter(obj => obj.id !== id)
-
-//             const newPuppies = [
-//                 ...filteredData,
-//                 { ...newdata }
-//             ]
-//             return newPuppies
-//         })
-//         // .then((newPuppies) => {
-//         //     return fs.writeFile('data.json', JSON.stringify(newPuppies, null, 2), 'utf8')
-//         // })
-//         // .then(()=>{
-//         //     return res.redirect(`/puppies/${id}`)
-//         // })
-//         // .catch((err) => {
-//         //     console.error(err);
-//         // })
-
-// })
-
-// server.post('/puppies/:id/edit', (req, res) => {
-//     const paramsid = Number(req.params.id)
-
-//     const newdata = JSON.parse(JSON.stringify(req.body, null, 2))
-//     newdata.id = paramsid
-
-//     dogData.puppies = dogData.puppies.filter(obj => obj.id !== paramsid)
-
-//     const list = dogData.puppies
-//     dogData.puppies = [
-//         ...list,
-//         { ...newdata }
-//     ]
-
-//     const filePath = path.join(__dirname, '/data.json')
-
-
-//     fs.writeFile(filePath, JSON.stringify(dogData, null, 2), 'utf8')
-//         .catch((err) => {
-//             console.error(err);
-//         })
-
-//     res.redirect(`/puppies/${paramsid}`)
-// })
+server.use(routertest)
+//class code랑 다른점은 Path를 넣지 않는다...
