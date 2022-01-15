4c68bd46f90caa69bbd1065256b3da16b323a10e
kate-turnbull <kate@kateturnbull.codes>
2022-01-15 15:31:39 +1300

    begin work on post puppies edit function

--- puppiesroutes.js
+++ puppiesroutes.js
@@ -26,6 +26,42 @@ router.get('/:id', (req, res) => {
     })
 })
 
+// GET puppies/:id/edit
+router.get('/:id/edit', (req, res) => {
+
+     // id === req.params.id
+     const id = Number(req.params.id) - 1
+
+    // write fs.promise readfile function 
+    fs.readFile('data.json', 'utf-8')
+    // loop through puppies
+    .then((puppiesArray) => { 
+        const data = JSON.parse(puppiesArray)
+        
+        const puppiesData = data.puppies.find( item => item.id -1 === id)
+        // send the right puppy
+        res.render('edit', puppiesData)
+        })
+        .catch(err => {
+            console.error('ops something happend ', err);
+        })
+
+})
+
+// POST puppies/:id/edit
+router.post('/:id/edit', (req, res) => {
+    // create object from req.body
+    console.log(req.body)
+
+    // {
+    //     "id": "req.body.id",
+    //     "name": "req.body.name",
+    //     "owner": "req.body.owner",
+    //     "breed": "req.body.breed"
+    //   }
+})
+
+
 module.exports = router
 
 
