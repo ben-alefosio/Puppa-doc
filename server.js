91080bc2f78e02d57da58f5ff4a49ee6b5ccdf42
ben daniels <bendaniels.take2@gmail.com>
2022-01-16 22:20:25 +1300

    added-catch

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-const express = require("express");
-const hbs = require("express-handlebars");
-const fsPromises = require("fs").promises;
+const express = require('express')
+const hbs = require('express-handlebars')
+const fsPromises = require('fs').promises
 
-const server = express();
-module.exports = server;
-const pupRoute = require("./routes");
+const server = express()
+module.exports = server
+const pupRoute = require('./routes')
 
 // Server configuration
-server.use(express.static("public"));
-server.use(express.urlencoded({ extended: false }));
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
 
 // Handlebars configuration
-server.engine("hbs", hbs({ extname: "hbs" }));
-server.set("view engine", "hbs");
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-server.use("/", pupRoute);
+server.use('/', pupRoute)
 
-server.get("/", (req, res) => {
-  fsPromises.readFile("data.json", "utf8").then((data) => {
-    const viewData = JSON.parse(data);
-    res.render("home", viewData);
-    return null;
-  });
-});
+server.get('/', (req, res) => {
+  fsPromises
+    .readFile('data.json', 'utf8')
+    .then((data) => {
+      const viewData = JSON.parse(data)
+      res.render('home', viewData)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+})
