80f418469710bf0ce8a4ad5450e72737de473cb3
cl-lin <clin3105@gmail.com>
2022-01-17 17:45:22 +1300

    User story 3 - GET route complete

--- routes.js
+++ routes.js
@@ -5,9 +5,8 @@ const router = express.Router()
 
 module.exports = router
 
+// Access to page - details.hbs
 router.get('/puppies/:id', (req, res) => {
-  // console.log('this router works!')
-  // res.send('this page works!')
   const id = Number(req.params.id)
 
   fsPromises.readFile('data.json', 'utf8')
@@ -23,5 +22,22 @@ router.get('/puppies/:id', (req, res) => {
     })
 })
 
+// Access to page - edit.hbs
+router.get('/puppies/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id)
+
+  fsPromises.readFile('data.json', 'utf8')
+    .then((data) => {
+      const pupData = JSON.parse(data)
+      const pupId = pupData.puppies.find(ele => ele.id === id)
+      res.render('edit', pupId)
+
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+})
 // const pup = puppies.find({req.params.id})
 // res.render ('puppy', pup)
