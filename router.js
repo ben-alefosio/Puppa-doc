c7e091bdce16758100c75bde7681ec193af194cc
campbell <campbell.mason@hotmail.co.nz>
2022-01-23 14:57:45 +1300

    edit puppies works

--- router.js
+++ router.js
@@ -32,6 +32,71 @@ router.get('/puppies/:id', (req, res) => {
     })
 })
 
+router.get('/puppies/:id/edit', (req, res) => {
+     fsPromises.readFile('data.json', 'utf8')
+    .then(data => {
+        const dogs = JSON.parse(data)
+        
+        for(let i=0; i<dogs.puppies.length; i++) {
+            if (dogs.puppies[i].id === parseInt(req.params.id)) { 
+                console.log('found '+JSON.stringify(dogs.puppies[i]))
+                return dogs.puppies[i]
+            }
+
+        }
+        // console.log("dogs.puppies.id  "+dogs.puppies[0].id)
+        // console.log("parsed "+JSON.stringify(JSON.parse(data)))
+        
+    })
+    .then(parsed => {
+          
+        return res.render('edit', parsed)
+        
+    })
+    .catch(error => {
+     console.log(error);
+    })
+    
+})
+
+router.post('/puppies/:id/edit', (req, res) => {
+    var requestBody = req.body
+    // console.log('post '+JSON.stringify(requestBody))
+
+    fsPromises.readFile('data.json', 'utf8')
+    .then(data => {
+        const dogs = JSON.parse(data)
+        
+        for(let i=0; i<dogs.puppies.length; i++) {
+            if (dogs.puppies[i].id === parseInt(req.params.id)) { 
+                console.log('found '+JSON.stringify(dogs.puppies[i]))
+                dogs.puppies[i].name = requestBody.name
+                
+            }
+
+        }
+        // console.log("dogs.puppies.id  "+dogs.puppies[0].id)
+        // console.log("dogs "+JSON.stringify(dogs))
+        return dogs
+        
+    })
+    .then(parsed => {
+        console.log("parsed "+JSON.stringify(parsed))
+
+        // return res.render('edit', parsed)
+        fsPromises.writeFile('data.json', JSON.stringify(parsed))
+        res.setHeader('Location','/puppies/'+req.params.id)
+        res.statusCode=302
+        return res.end();
+
+    })
+    
+    .catch(error => {
+     console.log(error);
+    })
+    
+})
+
 
 
 
