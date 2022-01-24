45c4ed0c5046afd40fc881e887c7b97796882898
angela-yen <angela.yen0210@gmail.com>
2022-01-24 22:08:31 +1300

    working server

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const path = require('path')
+
+const fs = require('fs').promises
+const puppiesData = path.join(__dirname, 'data.json')
 
 const server = express()
 
@@ -7,19 +11,23 @@ const server = express()
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
 
+server.get('/', (req, res) => {
+  fs.readFile(puppiesData, 'utf-8')
+    .then((puppiesArray) => {
+      const puppiesObj = JSON.parse(puppiesArray)
+
+      res.render('home', puppiesObj)
+      return null
+    })
+    .catch(err => {
+      console.error('opps something happend ', err)
+    })
+})
+
 // Handlebars configuration
 server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-server.get('/', (req, res) => {
-  const filepath = path.join(__dirname, 'data.json')
-
-  const pups = JSON.parse(data.Puppies)
-  const viewData = {
-    Puppies: pups
-  }
-  res.render('home', viewData)
-})
 
 module.exports = server
