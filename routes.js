29aa90e27a46a032df1f347c29227a515cdf1847
angela-yen <angela.yen0210@gmail.com>
2022-01-25 18:28:27 +1300

    working post route

--- routes.js
+++ routes.js
@@ -35,7 +35,7 @@ router.get('/:id/edit', (req, res) => {
       const puppiesObj = JSON.parse(data)
       // find the puppy that matches the id
       const findPuppy = puppiesObj.puppies.find(puppy => puppy.id === id)
-
+      // using the edit partial
       res.render('edit', findPuppy)
       return null
     })
@@ -45,7 +45,41 @@ router.get('/:id/edit', (req, res) => {
 })
 
 router.post('/:id/edit', (req, res) => {
+  // parseInt returns the first integer
+  const id = Number(req.params.id)
+  fs.readFile(puppiesData, 'utf-8')
+    .then((data) => {
+    // turn JSON file as regular file
+      const puppiesObj = JSON.parse(data)
+      // check to see if there is a puppy in the array that matches the id
+      const puppiesArray = puppiesObj.puppies.filter(puppy => JSON.stringify(puppy.id) === req.params.id)
+      //   console.log(puppiesArray)
+      // checking to see if there is a matching puppy
+      const noMatchingPuppy = puppiesObj.puppies.filter(puppy => JSON.stringify(puppy.id) !== req.params.id)
 
-}
+      //   console.log(noMatchingPuppy)
 
-)
+      // creating a new puppy object
+      const newPuppy = {
+        id: id,
+        image: puppiesArray[0].image,
+        breed: req.body.breed,
+        name: req.body.name,
+        owner: req.body.owner
+      }
+
+      //   console.log(newPuppy)
+      noMatchingPuppy.push(newPuppy)
+      const newPuppyData = { puppies: noMatchingPuppy }
+
+      //   console.log(newPuppyData)
+      fs.writeFile(puppiesData, JSON.stringify(newPuppyData), 'utf-8')
+      return null
+        .then(() => {
+          return res.redirect(`/puppies/${id}`)
+        })
+        .catch(err => {
+          console.error('opps something happend ', err)
+        })
+    })
+})
