91db64b8fb011aaa463dc2f23aab03466bd4509d
gus.hawke <gus.hawke@gmail.com>
2022-01-26 08:04:24 +1300

    update puppies

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const server = express()
 module.exports = router
 const path = require('path')
+
+// Accecpt url encoded data
+server.use(express.urlencoded({ extended: false }))
 
 const filepath = path.join(__dirname, 'data.json')
 
@@ -43,8 +47,60 @@ router.get('/puppies/:id', (req, res) => {
     })
 })
 
-router.get('/puppies/:id/edit', (req, res) => {
-  console.log('param', req.params.id)
-}
+// update puppy
 
-)
+router.get('/puppies/:id/edit', (req, res) => {
+  // console.log('param', req.params.id)
+
+  const id = Number(req.params.id - 1)
+
+  fsPromises.readFile(filepath, 'utf-8')
+    .then((data) => {
+      const result = JSON.parse(data)
+
+      res.render('edit', result.puppies[id])
+      return null
+    })
+    .catch(err => {
+      console.error(('ooopsy Daisy', err))
+    })
+})
+
+// POST route
+router.post('/puppies/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+
+  const newData = {
+    id: id,
+    name: req.body.name,
+    owner: req.body.owner,
+    image: req.body.image,
+    breed: req.body.breed
+  }
+  fsPromises.readFile(filepath, 'utf-8')
+    .then((data) => {
+      const result = JSON.parse(data)
+
+      const updatePup = result.puppies.map(pup => {
+        if (pup.id === id) {
+          pup = newData
+        }
+        return pup
+      })
+
+      result.puppies = updatePup
+      const newPup = JSON.stringify(result)
+
+      // .then MUST return the next promise if there's multiple (inside the .then '})' )
+
+      return fsPromises.writeFile('./data.json', newPup, 'utf-8')
+    })
+
+    .then(() => {
+      return res.redirect(`/puppies/${id}`)
+    })
+
+    .catch(err => {
+      console.error(err.message)
+    })
+})
