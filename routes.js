161ac646dfc9688e4e0a6fb6cb18f2b9f4639ac9
angela-yen <angela.yen0210@gmail.com>
2022-01-25 19:00:11 +1300

    with comments

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const fs = require('fs').promises
 const path = require('path')
 
+// puppiesData takes us to the path and is a string
 const puppiesData = path.join(__dirname, 'data.json')
 
 module.exports = router
@@ -18,10 +19,11 @@ router.get('/:id', (req, res) => {
       const puppiesObj = JSON.parse(data)
       // find the puppy that matches the id
       const findPuppy = puppiesObj.puppies.find(puppy => puppy.id === id)
-
+      // putting the data into the browser
       res.render('details', findPuppy)
       return null
     })
+    // error messaage
     .catch(err => {
       console.error('opps something happend ', err)
     })
@@ -51,15 +53,14 @@ router.post('/:id/edit', (req, res) => {
     .then((data) => {
     // turn JSON file as regular file
       const puppiesObj = JSON.parse(data)
-      // check to see if there is a puppy in the array that matches the id
+      // creating an array with 1 object element that filters the puppy that matched the id in the URL
       const puppiesArray = puppiesObj.puppies.filter(puppy => JSON.stringify(puppy.id) === req.params.id)
-      //   console.log(puppiesArray)
-      // checking to see if there is a matching puppy
+      // creating an array with the rest of the remainder puppies
       const noMatchingPuppy = puppiesObj.puppies.filter(puppy => JSON.stringify(puppy.id) !== req.params.id)
 
-      //   console.log(noMatchingPuppy)
-
       // creating a new puppy object
+      // req.body takes information that is put into the form on the browser
+      // Because puppiesArray only has 1 element in it, it will always be puppiesArray[0]
       const newPuppy = {
         id: id,
         image: puppiesArray[0].image,
@@ -68,13 +69,15 @@ router.post('/:id/edit', (req, res) => {
         owner: req.body.owner
       }
 
-      //   console.log(newPuppy)
+      // pushing the new puppy object with the new information recieved from the form into the array with the remainder of the puppies
       noMatchingPuppy.push(newPuppy)
       const newPuppyData = { puppies: noMatchingPuppy }
 
-      //   console.log(newPuppyData)
+      // returning a promise in a promise
+      // stringifying the new puppy data into JSON
       return fs.writeFile(puppiesData, JSON.stringify(newPuppyData), 'utf-8')
     })
+    // redirecting the back to the puppies id page
     .then(() => {
       return res.redirect(`/puppies/${id}`)
     })
