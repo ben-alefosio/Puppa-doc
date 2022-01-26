5e49ceebb3f9697ed8b762ecd9372328573e05be
ben daniels <bendaniels.take2@gmail.com>
2022-01-26 20:11:33 +1300

    user-story-2

--- routes.js
+++ routes.js
@@ -1,4 +1,20 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express()
+const fs = require('fs/promises')
 
-module.exports = router;
+router.get('/puppies/:id', (req, res) => {
+  const pupInfo = parseInt(req.params.id) - 1
+
+  fs.readFile('data.json')
+    .then((data) => {
+      return JSON.parse(data)
+    })
+    .then((result) => {
+      return res.render('details', result.puppies[pupInfo])
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+module.exports = router
