3b60788f6db1067d15570bd9c60b8e7ef55683ef
ben-alefosio <ben_alefosio@hotmail.com>
2022-01-29 12:45:51 +1300

    push branch

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require('express')
-const hbs = require('express-handlebars')
+// const express = require('express')
+// const hbs = require('express-handlebars')
+// const path = require('path')
 
-const server = express()
+// const server = express()
 
-// Server configuration
-server.use(express.static('public'))
-server.use(express.urlencoded({ extended: false }))
+// // Server configuration
+// server.use(express.static('public'))
+// server.use(express.urlencoded({ extended: false }))
 
-// Handlebars configuration
-server.engine('hbs', hbs({ extname: 'hbs' }))
-server.set('view engine', 'hbs')
+// // Handlebars configuration
+// server.engine('hbs', hbs({ extname: 'hbs' }))
+// server.set('view engine', 'hbs')
 
-// Your routes/router(s) should go here
+// // Your routes/router(s) should go here
 
+// // server.get('/', (req, res) => {
+// //     res.send('Pupparazzi')
+// // })
 
-module.exports = server
+// //include fs module
+// const fs = require('fs');
+// const fsPromises = require('fs').promises
+
+// //use fsPromises.readFile() method
+// // // to read the file
+
+// module.exports = server
