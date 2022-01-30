a0b11e20b7146a75e73449fe447d6e29d96ef248
root <root@DESKTOP-G6ANEQT.localdomain>
2022-01-31 08:10:22 +1300

    User story 1 complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const fs = require('fs/promises')
 
 const server = express()
 
@@ -12,9 +13,21 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-server.get('/', (req, res) => {
-    res.send('Pupparazzi')  //send is a function on res and it's going to send back a string.
-})
+// server.get('/', (req, res) => {
+//     res.send('Pupparazzi')  //send is a function on res and it's going to send back a string.
+// })
 
+server.get('/', (req, res) => {
+  fs.readFile('data.json')
+  .then(data => {
+      return JSON.parse(data)
+  })
+  .then(parsedData => {
+      return res.render('home', parsedData)
+  })
+  .catch(err => {
+      console.error('Check file name or path maybe? ', err.path)
+  })  
+})
 
 module.exports = server
