3fb9db74c02832a22a50a400d005ae31fcc6ef69
ben-alefosio <ben_alefosio@hotmail.com>
2022-02-09 15:26:02 +1300

    user story 3

--- routes.js
+++ routes.js
@@ -21,4 +21,49 @@ router.get('/puppies/:id', (req, res) => {
     })
 })
 
+router.get('/puppies/:id/edit', (req, res) => {
+  fs.readFile(puppiesData, 'utf8')
+    .then(data => {
+      const pups = JSON.parse(data)
+
+      return pups.puppies.find(puppy => puppy.id === parseInt(req.params.id))
+    })
+    .then(parsed => {
+      return res.render('edit', parsed)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+router.post('/puppies/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+
+  fs.readFile(puppiesData, 'utf8')
+    .then(data => {
+      const doggies = JSON.parse(data)
+      const doggiesArr = doggies.puppies.filter(puppy => JSON.stringify(puppy.id) === req.params.id)
+      const noMatch = doggies.puppies.filter(puppy => JSON.stringify(puppy.id) !== req.params.id)
+
+      const newDoggy = {
+        id: id,
+        image: doggiesArr[0].image,
+        breed: req.body.breed,
+        name: req.body.name,
+        owner: req.body.owner
+      }
+      noMatch.push(newDoggy)
+      const newDawg = { puppies: noMatch }
+      return fs.writeFile(puppiesData, JSON.stringify(newDawg)
+      )
+    })
+    .then(() => {
+      return res.redirect(`/puppies/${id}`)
+    })
+
+    .catch(error => {
+      console.log(error)
+    })
+})
+
 module.exports = router
